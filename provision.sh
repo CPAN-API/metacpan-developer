369d369481966e4b591d9fbe4ef60928653cc94d
Randy Stauner <randy@magnificent-tears.com>
2013-07-22 20:24:16 -0700

    Do the mount-tmp-dir-on-top trick for metacpan's var

--- provision.sh
+++ provision.sh
@@ -1,18 +1,22 @@
-# Break open the var cache here for metacpan.org
-mkdir -p /tmp/meta_var/tmp
-chmod -R 777 /tmp/meta_var
-ln -s /tmp/meta_var /home/metacpan/metacpan.org/var
+#!/bin/bash
 
-# Sort out puppet's ssl dir so it can be written to
-ssldir=/etc/puppet/ssl
-mkdir -p $ssldir
+# Vagrant shared directories get owned by the vagrant user
+# but some programs need certain (usually sub-) dirs to be user-writable.
+# Create tmp dirs and mount them on top of the share to work around this.
+function change_shared_dir_owner () {
+  local user="$1" dest="$2"
+  local src="/tmp/v-share-mounts/$dest"
+  mkdir -p "$dest" "$src"
+  chown "$user" "$src"
+  mount --bind "$src" "$dest"
+}
 
-ssl_bind_src=/etc/puppet-ssl
-mkdir -p $ssl_bind_src
-chown puppet:puppet $ssl_bind_src
+# The web app writes to ./var/tmp
+change_shared_dir_owner metacpan /home/metacpan/metacpan.org/var
 
-mount --bind $ssl_bind_src $ssldir
-chown -R puppet:puppet $ssldir
+# Puppet needs to own it's ssl dir.
+change_shared_dir_owner puppet:puppet /etc/puppet/ssl
+
 
 # Run puppet to cleanup and check everything is running
 /etc/puppet/run.sh
