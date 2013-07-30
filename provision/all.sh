bb6a9868b6d0cfdc633a40fc788dfdb6f1a3de27
Randy Stauner <randy@magnificent-tears.com>
2013-07-30 07:33:52 -0700

    Set up dirs to break out before/after puppet scripts
    to help clean up the main provisioner and make testing easier.

--- provision/all.sh
+++ provision/all.sh
@@ -40,5 +40,27 @@ chmod -R g+w /home/metacpan/api.metacpan.org/{t/,}var/tmp
 change_shared_dir_owner puppet:puppet /etc/puppet/ssl
 
 
+# Execute all the scripts in a directory.
+function exec_dir () {
+  local dir="$1"
+  [[ "${dir:0:1}" == "/" ]] || dir="/vagrant/provision/$dir"
+  for exe in "$dir"/*; do
+    test -x "$exe" && echo "$exe" && "$exe"
+  done
+}
+
+
+# Execute any before-puppet vm initialization scripts.
+exec_dir before
+
+
 # Run puppet to cleanup and check everything is running
 /etc/puppet/run.sh
+
+
+# Execute any after-puppet vm clean-up scripts.
+exec_dir after
+
+
+# Don't let vagrant think the provision failed.
+exit 0
