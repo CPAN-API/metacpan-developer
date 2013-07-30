13ea7734ff627d31fa08f205f1d99c525c684fb8
Randy Stauner <randy@magnificent-tears.com>
2013-07-29 23:09:12 -0700

    Fix directories for the api test suite

--- provision.sh
+++ provision.sh
@@ -5,6 +5,7 @@
 grep -qE '^search ' /etc/resolv.conf || \
   echo 'search metacpan.org' >> /etc/resolv.conf
 
+
 # Vagrant shared directories get owned by the vagrant user
 # but some programs need certain (usually sub-) dirs to be user-writable.
 # Create tmp dirs and mount them on top of the share to work around this.
@@ -23,6 +24,17 @@ function change_shared_dir_owner () {
 
 # The web app writes to ./var/tmp
 change_shared_dir_owner metacpan /home/metacpan/metacpan.org/var
+
+# API test suite writes to multiple dirs.
+change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/t/var/tmp
+change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/var/tmp
+
+# CPAN::Faker 0.008 requires source dir to be writable (not sure why).
+# https://github.com/CPAN-API/cpan-api/issues/277
+# https://github.com/rjbs/CPAN-Faker/issues/1
+# In the meantime the test suite can be run as the vagrant user.
+chmod -R g+w /home/metacpan/api.metacpan.org/{t/,}var/tmp
+
 
 # Puppet needs to own it's ssl dir.
 change_shared_dir_owner puppet:puppet /etc/puppet/ssl
