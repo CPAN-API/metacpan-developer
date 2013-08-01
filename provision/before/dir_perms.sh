361c26b038421cfa21400edf343be093ace988ef
Randy Stauner <randy@magnificent-tears.com>
2013-08-01 15:23:53 -0700

    Move dir perm function to a separate file

--- provision/before/dir_perms.sh
+++ provision/before/dir_perms.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Vagrant shared directories get owned by the vagrant user
-# but some programs need certain (usually sub-) dirs to be user-writable.
-# Create tmp dirs and mount them on top of the share to work around this.
-function change_shared_dir_owner () {
-  local user="$1" dest="$2"
-  local src="/tmp/v-share-mounts/$dest"
-
-  mkdir -p "$dest" "$src"
-  chown "$user" "$src"
-
-  # Only proceed if not already mounted.
-  mount | grep -qF " $dest " && return
-
-  mount --bind "$src" "$dest"
-}
+# Get functions.
+. /vagrant/provision/common.bashrc
 
 
 # The web app writes to ./var/tmp
