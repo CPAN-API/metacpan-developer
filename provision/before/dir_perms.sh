ed1c02d81cff196c2439b780f11aba40705758b7
Randy Stauner <randy@magnificent-tears.com>
2013-08-01 15:25:11 -0700

    Work harder to ensure metacpan or vagrant can use api
    Ideally the 'metacpan' user would be used,
    but because of the previously discovered issue in CPAN::Faker
    it's much easier to be able to run the test suite as 'vagrant'
    but the service will still run as 'metacpan'
    so try to make it usable by both.

--- provision/before/dir_perms.sh
+++ provision/before/dir_perms.sh
@@ -11,13 +11,22 @@ change_shared_dir_owner metacpan /home/metacpan/metacpan.org/var
 # API test suite writes to multiple dirs.
 change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/t/var/tmp
 change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/var/
-sudo -u metacpan mkdir -p /home/metacpan/api.metacpan.org/var/{tmp,log}
 
 # CPAN::Faker 0.008 mistakenly requires source dir to be writable.
 # https://github.com/CPAN-API/cpan-api/issues/277
 # https://github.com/rjbs/CPAN-Faker/issues/1
 # In the meantime the test suite can be run as the vagrant user.
-chmod -R g+w /home/metacpan/api.metacpan.org/{t/,}var/tmp
+
+# Make the required subdirs so that we can grant write perms to the group.
+api_root=/home/metacpan/api.metacpan.org
+api_dirs=($api_root/{t/,}var/{tmp,log})
+  mkdir -p "${api_dirs[@]}"
+
+  # Ensure file is created so vagrant or metacpan can keep writing to it.
+  touch $api_root/var/log/metacpan.log
+
+  chown -R metacpan:vagrant "${api_dirs[@]}"
+  chmod -R g+w "${api_dirs[@]}"
 
 
 # Puppet needs to own it's ssl dir.
