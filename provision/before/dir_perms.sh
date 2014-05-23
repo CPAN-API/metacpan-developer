7516fbf4599d0cbd0e2c51dba401efb28ffaf6e9
Olaf Alders <olaf@wundersolutions.com>
2014-05-22 22:47:22 -0400

    No need to chown app folders anymore.

--- provision/before/dir_perms.sh
+++ provision/before/dir_perms.sh
@@ -3,15 +3,5 @@
 # Get functions.
 . /vagrant/provision/common.bashrc
 
-
-# The web app writes to ./var/tmp
-change_shared_dir_owner metacpan /home/metacpan/metacpan.org/var
-
-
-# API test suite writes to multiple dirs.
-change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/t/var/tmp
-change_shared_dir_owner metacpan:vagrant /home/metacpan/api.metacpan.org/var/
-
-
 # Puppet needs to own it's ssl dir.
 change_shared_dir_owner puppet:puppet /etc/puppet/ssl
