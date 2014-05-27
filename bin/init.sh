56b5df2670ad6b9d926ba33d93aada81cae110aa
Olaf Alders <olaf@wundersolutions.com>
2014-05-27 18:51:44 -0400

    Enable pre-commit hooks in init script.

--- bin/init.sh
+++ bin/init.sh
@@ -8,4 +8,11 @@ git clone git://github.com/CPAN-API/cpan-api.git
 git clone git://github.com/CPAN-API/metacpan-web.git
 git clone git://github.com/CPAN-API/metacpan-explorer.git
 
+cd metacpan-web
+sh git/setup.sh
+cd ..
+
+cd cpan-api
+sh git/setup.sh
+
 vagrant plugin install vagrant-vbguest
