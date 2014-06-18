895252e206b7639bc7b8bc569f9a64d8c7041102
Randy Stauner <randy@magnificent-tears.com>
2014-06-18 09:15:26 -0700

    Use env var to DRY partial mirror script

--- bin/partial-cpan-mirror.sh
+++ bin/partial-cpan-mirror.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-mkdir -p ~/CPAN
+MINICPAN=$HOME/CPAN
+mkdir -p $MINICPAN
+
 RSYNC='/usr/bin/rsync -av --delete --relative'
 PATH='cpan-rsync.perl.org::CPAN'
 
-$RSYNC $PATH/authors/id/L/LL/LLAP ~/CPAN/
-$RSYNC $PATH/authors/id/N/NE/NEILB ~/CPAN/
-$RSYNC $PATH/authors/id/O/OA/OALDERS ~/CPAN/
-$RSYNC $PATH/authors/id/P/PE/PERLER ~/CPAN/
-$RSYNC $PATH/authors/id/R/RW/RWSTAUNER ~/CPAN/
+$RSYNC $PATH/authors/id/L/LL/LLAP       $MINICPAN/
+$RSYNC $PATH/authors/id/N/NE/NEILB      $MINICPAN/
+$RSYNC $PATH/authors/id/O/OA/OALDERS    $MINICPAN/
+$RSYNC $PATH/authors/id/P/PE/PERLER     $MINICPAN/
+$RSYNC $PATH/authors/id/R/RW/RWSTAUNER  $MINICPAN/
 
-$RSYNC $PATH/authors/00whois.xml ~/CPAN/
-$RSYNC $PATH/modules/0* ~/CPAN/
+$RSYNC $PATH/authors/00whois.xml        $MINICPAN/
+$RSYNC $PATH/modules/0*                 $MINICPAN/
