8bb7db65fd417429530e1b156a3625b2eb9986d8
Thomas Sibley <tom@zulutango.org>
2016-11-19 10:03:00 -0800

    Provision the VM with the current Puppet Labs package signing key
    Otherwise the "aptitude update" and "upgrade" steps don't fully work
    because apt can't verify the packages.

--- provision/before/apt.sh
+++ provision/before/apt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo sed -i 's,http://debian.yorku.ca/debian/,http://mirror.bytemark.co.uk/debian/,' /etc/apt/sources.list
+sudo apt-key adv --batch --keyserver pgp.mit.edu --recv-key '6F6B 1550 9CF8 E59E 6E46  9F32 7F43 8280 EF8D 349F'
 sudo aptitude update
 sudo aptitude upgrade -y
