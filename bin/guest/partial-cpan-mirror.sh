2cca9def3daa9f3ca75ec81532794356813b0c82
Graham Knop <haarg@haarg.org>
2019-04-13 23:26:22 +0200

    use better variable name for cpan root in sync script

--- bin/guest/partial-cpan-mirror.sh
+++ bin/guest/partial-cpan-mirror.sh
@@ -4,15 +4,15 @@ MINICPAN=$HOME/CPAN
 mkdir -p $MINICPAN
 
 RSYNC='/usr/bin/rsync -av --delete --relative'
-PATH='cpan-rsync.perl.org::CPAN'
+CPAN_ROOT='cpan-rsync.perl.org::CPAN'
 
-$RSYNC $PATH/authors/id/L/LL/LLAP       $MINICPAN/
-$RSYNC $PATH/authors/id/N/NE/NEILB      $MINICPAN/
-$RSYNC $PATH/authors/id/O/OA/OALDERS    $MINICPAN/
-$RSYNC $PATH/authors/id/P/PE/PERLER     $MINICPAN/
-$RSYNC $PATH/authors/id/R/RW/RWSTAUNER  $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/id/L/LL/LLAP       $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/id/N/NE/NEILB      $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/id/O/OA/OALDERS    $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/id/P/PE/PERLER     $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/id/R/RW/RWSTAUNER  $MINICPAN/
 
-$RSYNC $PATH/authors/0*                 $MINICPAN/
-$RSYNC $PATH/modules/0*                 $MINICPAN/
+$RSYNC $CPAN_ROOT/authors/0*                 $MINICPAN/
+$RSYNC $CPAN_ROOT/modules/0*                 $MINICPAN/
 
-$RSYNC $PATH/indices/mirrors.json       $MINICPAN/
+$RSYNC $CPAN_ROOT/indices/mirrors.json       $MINICPAN/
