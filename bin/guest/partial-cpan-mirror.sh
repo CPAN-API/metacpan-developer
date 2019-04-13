799f1b1a9bf3ca76a566a19a79a5d1415cf18f03
Graham Knop <haarg@haarg.org>
2019-04-13 23:26:42 +0200

    quote variables in sync script

--- bin/guest/partial-cpan-mirror.sh
+++ bin/guest/partial-cpan-mirror.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-MINICPAN=$HOME/CPAN
-mkdir -p $MINICPAN
+MINICPAN="$HOME/CPAN"
+mkdir -p "$MINICPAN"
 
 RSYNC='/usr/bin/rsync -av --delete --relative'
 CPAN_ROOT='cpan-rsync.perl.org::CPAN'
 
-$RSYNC $CPAN_ROOT/authors/id/L/LL/LLAP       $MINICPAN/
-$RSYNC $CPAN_ROOT/authors/id/N/NE/NEILB      $MINICPAN/
-$RSYNC $CPAN_ROOT/authors/id/O/OA/OALDERS    $MINICPAN/
-$RSYNC $CPAN_ROOT/authors/id/P/PE/PERLER     $MINICPAN/
-$RSYNC $CPAN_ROOT/authors/id/R/RW/RWSTAUNER  $MINICPAN/
+$RSYNC "$CPAN_ROOT/authors/id/L/LL/LLAP"       "$MINICPAN/"
+$RSYNC "$CPAN_ROOT/authors/id/N/NE/NEILB"      "$MINICPAN/"
+$RSYNC "$CPAN_ROOT/authors/id/O/OA/OALDERS"    "$MINICPAN/"
+$RSYNC "$CPAN_ROOT/authors/id/P/PE/PERLER"     "$MINICPAN/"
+$RSYNC "$CPAN_ROOT/authors/id/R/RW/RWSTAUNER"  "$MINICPAN/"
 
-$RSYNC $CPAN_ROOT/authors/0*                 $MINICPAN/
-$RSYNC $CPAN_ROOT/modules/0*                 $MINICPAN/
+$RSYNC "$CPAN_ROOT/authors/0*"                 "$MINICPAN/"
+$RSYNC "$CPAN_ROOT/modules/0*"                 "$MINICPAN/"
 
-$RSYNC $CPAN_ROOT/indices/mirrors.json       $MINICPAN/
+$RSYNC "$CPAN_ROOT/indices/mirrors.json"       "$MINICPAN/"
